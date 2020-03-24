83b6045221acb7710d63899e7ab0c1c3825bfbe4
dibressonCosta <luizgustavo.costa@hotmail.com>
2020-03-23 23:22:22 -0300

    Quase pronto algoritmo DFS

--- maze/src/maze/NodeMaze.groovy
+++ maze/src/maze/NodeMaze.groovy
@@ -3,32 +3,26 @@ package maze
 class NodeMaze extends Node{
 	int x;
 	int y;
-	public NodeMaze(Node Parent, Maze maze, int x, int y) {
+	public NodeMaze(Node parent, int x, int y) {
 		super();
 		this.x = x
 		this.y = y
-		this.parent = Parent
+		this.parent = parent
+	}
+	public NodeMaze(int x, int y) {
+		super()
+		this.x = x
+		this.y = y
 	}
 	
-	private enum DIR {
-		D(1, 0), C(0, 1), B(0, -1), E(-1, 0);
-		private final int bit;
-		private final int dx;
-		private final int dy;
-		private DIR opposite;
- 
-		// use the static initializer to resolve forward references
-		static {
-			C.opposite = B;
-			B.opposite = C;
-			E.opposite = D;
-			D.opposite = E;
-		}
-	}
+	//static def Directions = ["D": [1,0], "E":[-1,0],"C":[0,1], "B": [0,-1]]
 
 	@Override
 	public Boolean isGoal() {
-		return false;
+		if(this.x == 31 && this.y ==15) {
+			return true
+		}else
+			return false
 	}
 	
 	@Override
--- maze/src/maze/TestMaze.groovy
+++ maze/src/maze/TestMaze.groovy
@@ -1,27 +1,143 @@
 package maze
 
-import maze.NodeMaze.DIR
+
 
 class TestMaze {
-	
-	private static int[][] DIRECTIONS = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-	
-	private static DIR = {[0, 1 ] [ 1, 0 ] [ 0, -1 ] [ -1, 0 ] }
-	
+	static def u = 0
+	static def Directions = ["R": [1, 0], "L":[-1, 0],"U":[0, -1], "D": [0, 1]]
+	static Boolean TestRight(NodeMaze No, Maze maze) {
+		int x = No.x
+		int y = No.y
+		int aux_x = x + Directions["R"][0]
+		int aux_y = y + Directions["R"][1]
+		return maze.isEmpty(aux_x, aux_y)
+	}
+	static Boolean TestLeft(NodeMaze No, Maze maze) {
+		int x = No.x
+		int y = No.y
+		int aux_x = x + Directions["L"][0]
+		int aux_y = y + Directions["L"][1]
+		return maze.isEmpty(aux_x, aux_y)
+	}
+	static Boolean TestUp(NodeMaze No, Maze maze) {
+		int x = No.x
+		int y = No.y
+		int aux_x = x + Directions["U"][0]
+		int aux_y = y + Directions["U"][1]
+		return maze.isEmpty(aux_x, aux_y)
+	}
+	static Boolean TestDown(NodeMaze No, Maze maze) {
+		int x = No.x
+		int y = No.y
+		int aux_x = x + Directions["D"][0]
+		int aux_y = y + Directions["D"][1]
+		return maze.isEmpty(aux_x, aux_y)
+	}
+	static Node nextCoordinate(NodeMaze No, Maze maze) {
+		if(u == 0) {
+			if(TestRight(No, maze)) {
+				NodeMaze nextNode = new NodeMaze(No, No.x + Directions["R"][0], No.y + Directions["R"][1])
+				maze.addPoint(nextNode.x, nextNode.y)
+				return nextNode
+			}else if(TestDown(No, maze)) {
+				NodeMaze nextNode = new NodeMaze(No, No.x + Directions["D"][0], No.y + Directions["D"][1])
+				maze.addPoint(nextNode.x, nextNode.y)
+				return nextNode
+			}else if(TestLeft(No, maze)) {
+				NodeMaze nextNode = new NodeMaze(No, No.x + Directions["L"][0], No.y + Directions["L"][1])
+				maze.addPoint(nextNode.x, nextNode.y)
+				return nextNode
+			}else if(TestUp(No, maze)) {
+				u = 1
+				NodeMaze nextNode = new NodeMaze(No, No.x + Directions["U"][0], No.y + Directions["U"][1])
+				maze.addPoint(nextNode.x, nextNode.y)
+				return nextNode
+			}
+		}else {
+			if(TestUp(No, maze)) {
+				NodeMaze nextNode = new NodeMaze(No, No.x + Directions["U"][0], No.y + Directions["U"][1])
+				maze.addPoint(nextNode.x, nextNode.y)
+				return nextNode
+			}else if(TestRight(No, maze)) {
+				u = 0
+				NodeMaze nextNode = new NodeMaze(No, No.x + Directions["R"][0], No.y + Directions["R"][1])
+				maze.addPoint(nextNode.x, nextNode.y)
+				return nextNode
+			}else if(TestDown(No, maze)) {
+				u = 0
+				NodeMaze nextNode = new NodeMaze(No, No.x + Directions["D"][0], No.y + Directions["D"][1])
+				maze.addPoint(nextNode.x, nextNode.y)
+				return nextNode
+			}else if(TestLeft(No, maze)) {
+				u = 0
+				NodeMaze nextNode = new NodeMaze(No, No.x + Directions["L"][0], No.y + Directions["L"][1])
+				maze.addPoint(nextNode.x, nextNode.y)
+				return nextNode
+			}
+		}
+	}
+	static Boolean explore(Maze maze, NodeMaze no, path) {
+		maze.display()
+		sleep(200)
+
+		if(no == null) {
+			return true
+		}
+		path.add(no)
+		if (no.x>maze.getNCols())
+			return false;
+		if (no.y>maze.getNRows())
+			return false;
+		if(no.x == 31 && no.y == 15) {
+			return true
+		}
+		for (int i = 0; i < 4; i++) {
+			NodeMaze node = nextCoordinate(no,maze);
+			if (explore(maze, node, path)) {
+				maze.addPoint(node.x, node.y)
+				return true;
+			}
+		}
+		//path.remove(path.size() - 1);
+		return false;
+	}
 	static void main(String[] args) {
-		
+		def tom = []
+		def path = []
 		final int x = args.length >= 1 ? Integer.parseInt(args[0]) : 8;
 		final int y = args.length == 2 ? Integer.parseInt(args[1]) : 8;
 		final Maze maze = new Maze(x, y,1,1,31,15);
 		//maze.display();
-		
-		maze.addPoint(31,15);
+		def point = [1, 1]
+		//maze.addPoint(31,15);
+		NodeMaze goal = new NodeMaze(31,15)
 		maze.addPoint(1,1);
 		//maze.display();
-		
 		//int test = maze.getOriginX()
-		print DIR
-		
+		NodeMaze no = new NodeMaze(1,1)
+		maze.display()
+		explore(maze, no, path)
+
+		maze.display()
+
+
+
+		//		println " "
+		//		println TestRight(no, maze)
+		//		println TestLeft(no, maze)
+		//		println TestUp(no, maze)
+		//		println TestDown(no, maze)
+
+		//maze.display()
+		//NodeMaze noAux = no2
+
+		//NodeMaze no2 = nextCoordinate(no, maze)
+		//explore(maze, no2, path)
+		//NodeMaze no3 = nextCoordinate(no2, maze)
+		//NodeMaze no4 = nextCoordinate(no3, maze)
+		//NodeMaze no5 = nextCoordinate(no4, maze)
+		//NodeMaze no6 = nextCoordinate(no5, maze)
+		//maze.display()
+		//println no6.parent
 	}
-	
 }
